be4b34514595b3cf9d28f17b19aed8ccece711b8
Isaac Pearl <pearl.isaac@gmail.com>
2019-07-19 13:24:29 -0400

    ipc usage example

--- PoolsApp.js
+++ PoolsApp.js
@@ -3,16 +3,34 @@ const Drop = require("./src/Drop.js");
 const Event = require("./src/Event.js");
 const Pool = require("./src/Pool.js");
 
+const fs = require("fs");
+const ipcMain = require('electron').ipcMain;
 const SerialPort = require('./node_modules/serialport');
 const Readline = require('./node_modules/@serialport/parser-readline');
 const crowPort = new SerialPort('/dev/ttyACM0', {
 	baudRate: 115200 
 })
 
-const fs = require("fs");
-
 function getStateScript(){
-	return fs.readFileSync("src/state.lua", "utf8");
+	var script = fs.readFileSync("src/state.lua", "utf8");
+	console.log(script);
+	return script;
 }
 
-Crow.run(crowPort, getStateScript());
+/*
+IPC USAGE:
+
+ipcMain.on('asynchronous-message', (event, arg) => {
+  console.log(arg) // prints "ping"
+  event.reply('asynchronous-reply', 'pong')
+})
+
+ipcMain.on('synchronous-message', (event, arg) => {
+  console.log(arg) // prints "ping"
+  event.returnValue = 'pong'
+})
+*/
+
+ipcMain.on('run-script', (event, arg) => {
+	Crow.run(crowPort, getStateScript());
+});
--- renderer.js
+++ renderer.js
@@ -2,76 +2,30 @@
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
 
-const SerialPort = require('./node_modules/serialport');
-const Readline = require('./node_modules/@serialport/parser-readline');
-/*const crowPort = new SerialPort('/dev/ttyACM0', {
-	baudRate: 115200 
-})
-*/
-function getVolts2(crowInput) {
-	var luaString = `print(input[${crowInput}]())\n`;
-	crowPort.write(luaString, function(err) {
-		if (err) {
-			return console.log('Error on write: ', err.message)
-		}
-		//console.log('message written')
-	});
-
-	crowPort.on('readable', function() {
-		console.log('data: ', crowPort.read());
-	});
-}
-
-function getVolts(crowInput) {
-	var luaString = `print(input[${crowInput}]())\n`;
-	crowPort.write(luaString, function(err) {
-		if (err) {
-			return console.log('Error on write: ', err.message)
-		}
-		//console.log('message written')
-	});
-
-	/* Read data that is available but keep the stream in "paused mode"
-	crowPort.on('readable', function () {
-		console.log('Data:', crowPort.read())
-	});
-	
-
-	Switches the port into "flowing mode"
-	crowPort.on('data', function (data) {
-		console.log('Data:', data);
-	});
-	*/
-
-	// Pipe the data into another stream (like a parser or standard out)
-	
-	const lineStream = crowPort.pipe(new Readline());
-	var funcCounter = 0
-	lineStream.on('data', function(data) {
-		console.log(`input: ${crowInput}. counter: ${funcCounter}. data: ${data}.`);
-		funcCounter += 1;
-		drawInput(data, crowInput);
-	});
-
-	return lineStream;
-}
-
-function drawInput(val, inputNum) {
-	document.getElementById(`input${inputNum}`).innerHTML = val;
-}
-
-window.setInterval(function(){
-	//call this code every 500 milliseconds
-	//getVolts(1);
-	//getVolts(2);
-},1000);
+const ipc = require('electron').ipcRenderer;
 
 /*
-for (var i=0; i < 1; i++) {
-	console.log("running in a loop!");
-	getVolts2(1);
-	getVolts2(2);
-}
+IPC USAGE:
+
+console.log(ipc.sendSync('synchronous-message', 'ping')) // prints "pong"
+
+ipc.on('asynchronous-reply', (event, arg) => {
+  console.log(arg) // prints "pong"
+})
+
+ipc.send('asynchronous-message', 'ping')
 */
 
-//clearInterval();
+var refreshButton = document.getElementById("refresh-button");
+
+refreshButton.addEventListener('click', function(){
+	ipc.send('run-script', 1);
+});
+
+/*
+window.setInterval(function(){
+
+},1000);
+
+clearInterval();
+*/
