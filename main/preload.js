cb08f595ecaab14b55d659ddf9c38ea4c564c93e
Isaac Pearl <pearl.isaac@gmail.com>
2019-07-30 17:10:25 -0400

    added react project, connected react to electron server

--- main/preload.js
+++ main/preload.js
@@ -1,5 +1,8 @@
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
 window.addEventListener('DOMContentLoaded', () => {
-  })
+
+})
+
+window.electron = require('electron');
 
