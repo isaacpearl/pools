fb95d9b29376319336b6440ccda6a3221daa47df
Isaac Pearl <pearl.isaac@gmail.com>
2019-09-06 00:08:53 -0400

    added negative integer handling for time argument, resolves #75

--- main/src/State/State.lua
+++ main/src/State/State.lua
@@ -27,8 +27,9 @@ function translate(value, type)
 	local beat = 60/bpm --seconds of one beat in 4/4
 	local tVal = value
 	if type == "time" then
+		tVal = math.abs(tVal)
 		if value < 1 then
-			tVal = 1 --prevents 0 time infinite loop
+			tVal = 1 --prevents 0 time infinite loop for now, TODO add infinite hold on 0 until drop changes
 		else
 			tVal = 4 * (beat * (1/value))
 		end
